f3d3a6ea30295b6af9296d55f3a51fd7f01bf0a9
Cen1024 <2108664396@qq.com>
2025-11-28 22:05:46 +0800

    part A MainActivitty
    Signed-off-by: Cen1024 <2108664396@qq.com>

---
+++ app/src/main/java/com/example/lab2/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.lab2;
+
+import android.os.Bundle;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void onClickPlay(View view) {
+        Intent intent = new Intent(this, GameActivity.class);
+        startActivity(intent);
+    }
+
+    public void onClickLeaderboard(View view) {
+        Intent intent = new Intent(this, LeaderboardActivity.class);
+        startActivity(intent);
+    }
+}
